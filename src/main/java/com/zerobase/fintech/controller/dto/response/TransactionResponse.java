3e519b505b61feb6fd90c8455eb4a8226bd23e0e
YSMin <zxczxv7624@gmail.com>
2025-01-13 19:49:48 +0900

    [#3] 코드 개선
    - Response DTO 개선
    - 불변성 유지를 위해 record 사용
    - setter 사용 지양

--- src/main/java/com/zerobase/fintech/controller/dto/response/TransactionResponse.java
+++ src/main/java/com/zerobase/fintech/controller/dto/response/TransactionResponse.java
@@ -3,112 +3,96 @@ package com.zerobase.fintech.controller.dto.response;
 import com.zerobase.fintech.common.enums.TransactionStatus;
 import com.zerobase.fintech.common.enums.TransactionType;
 import com.zerobase.fintech.entity.TransactionEntity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Data
 public class TransactionResponse {
 
-    @Data
-    public static class BaseResponse {
-
-        private String amount;
-        private String balance;
-        private String memo;
-        private TransactionType transactionType;
-        private TransactionStatus transactionStatus;
-
-        public void populateCommonFields(TransactionEntity transaction) {
-            this.amount = transaction.getAmount().toString();
-            this.memo = transaction.getMemo();
-            this.transactionType = transaction.getTransactionType();
-            this.transactionStatus = transaction.getTransactionStatus();
-        }
-    }
-
-    @Data
-    @EqualsAndHashCode(callSuper = true)
-    public static class DepositResponse extends BaseResponse {
-        private String receiverAccountNumber;
-
+    public record DepositResponse(
+            String receiverAccountNumber,
+            String amount,
+            String balance,
+            String memo,
+            TransactionType transactionType,
+            TransactionStatus transactionStatus
+    ) {
         public static DepositResponse of(TransactionEntity transaction) {
-            DepositResponse response = new DepositResponse();
-
-            response.populateCommonFields(transaction);
-            response.receiverAccountNumber = transaction.getReceiverAccount().getAccountNumber();
-            response.setBalance(transaction.getReceiverAccount().getBalance().toString());
-
-            return response;
+            return new DepositResponse(
+                    transaction.getReceiverAccount().getAccountNumber(),
+                    transaction.getAmount().toString(),
+                    transaction.getReceiverAccount().getBalance().toString(),
+                    transaction.getMemo(),
+                    transaction.getTransactionType(),
+                    transaction.getTransactionStatus()
+            );
         }
     }
 
-    @Data
-    @EqualsAndHashCode(callSuper = true)
-    public static class WithdrawalResponse extends BaseResponse {
-        private String senderAccountNumber;
-
+    public record WithdrawalResponse(
+            String senderAccountNumber,
+            String amount,
+            String balance,
+            String memo,
+            TransactionType transactionType,
+            TransactionStatus transactionStatus
+    ) {
         public static WithdrawalResponse of(TransactionEntity transaction) {
-            WithdrawalResponse response = new WithdrawalResponse();
-
-            response.populateCommonFields(transaction);
-            response.senderAccountNumber = transaction.getSenderAccount().getAccountNumber();
-            response.setBalance(transaction.getSenderAccount().getBalance().toString());
-
-            return response;
+            return new WithdrawalResponse(
+                    transaction.getSenderAccount().getAccountNumber(),
+                    transaction.getAmount().toString(),
+                    transaction.getSenderAccount().getBalance().toString(),
+                    transaction.getMemo(),
+                    transaction.getTransactionType(),
+                    transaction.getTransactionStatus()
+            );
         }
     }
 
-    @Data
-    @EqualsAndHashCode(callSuper = true)
-    public static class TransferResponse extends BaseResponse {
-        private String senderAccountNumber;
-        private String receiverAccountNumber;
-
+    public record TransferResponse(
+            String senderAccountNumber,
+            String receiverAccountNumber,
+            String amount,
+            String balance,
+            String memo,
+            TransactionType transactionType,
+            TransactionStatus transactionStatus
+    ) {
         public static TransferResponse of(TransactionEntity transaction) {
-            TransferResponse response = new TransferResponse();
-
-            response.populateCommonFields(transaction);
-            response.senderAccountNumber = transaction.getSenderAccount().getAccountNumber();
-            response.receiverAccountNumber = transaction.getReceiverAccount().getAccountNumber();
-            response.setBalance(transaction.getSenderAccount().getBalance().toString());
-
-            return response;
+            return new TransferResponse(
+                    transaction.getSenderAccount().getAccountNumber(),
+                    transaction.getReceiverAccount().getAccountNumber(),
+                    transaction.getAmount().toString(),
+                    transaction.getSenderAccount().getBalance().toString(),
+                    transaction.getMemo(),
+                    transaction.getTransactionType(),
+                    transaction.getTransactionStatus()
+            );
         }
     }
 
-    @Data
-    public static class HistoryResponse {
-
-        private String amount;
-        private String balance;
-        private String memo;
-
+    public record HistoryResponse(
+            String amount,
+            String balance,
+            String memo
+    ) {
         public static List<HistoryResponse> of(List<TransactionEntity> transactions, String targetAccountNumber) {
-            List<HistoryResponse> responses = new ArrayList<>();
-
-            for (TransactionEntity transaction : transactions) {
-                HistoryResponse response = new HistoryResponse();
-
-                response.amount = transaction.getAmount().toString();
-
-                // 요청된 계좌의 잔액만 선택
-                if (transaction.getSenderAccount() != null &&
-                        transaction.getSenderAccount().getAccountNumber().equals(targetAccountNumber)) {
-                    response.balance = transaction.getSnapshot().getSenderBalanceAfter().toString();
-                } else if (transaction.getReceiverAccount() != null &&
-                        transaction.getReceiverAccount().getAccountNumber().equals(targetAccountNumber)) {
-                    response.balance = transaction.getSnapshot().getReceiverBalanceAfter().toString();
-                }
-
-                response.memo = transaction.getMemo();
-
-                responses.add(response);
-            }
-
-            return responses;
+            return transactions.stream()
+                    .map(transaction -> {
+                        String balance = null;
+                        if (transaction.getSenderAccount() != null &&
+                                transaction.getSenderAccount().getAccountNumber().equals(targetAccountNumber)) {
+                            balance = transaction.getSnapshot().getSenderBalanceAfter().toString();
+                        } else if (transaction.getReceiverAccount() != null &&
+                                transaction.getReceiverAccount().getAccountNumber().equals(targetAccountNumber)) {
+                            balance = transaction.getSnapshot().getReceiverBalanceAfter().toString();
+                        }
+                        return new HistoryResponse(
+                                transaction.getAmount().toString(),
+                                balance,
+                                transaction.getMemo()
+                        );
+                    })
+                    .toList();
         }
     }
 }
